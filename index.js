7f4ee9e0d6b18c93807785308bde8e3318b01506
Walter Hoerman <whoerman@gmail.com>
2020-03-10 15:25:57 -0400

    starting up a server

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var $noteTitle = $(".note-title");
-var $noteText = $(".note-textarea");
-var $saveNoteBtn = $(".save-note");
-var $newNoteBtn = $(".new-note");
-var $noteList = $(".list-container .list-group");
+const $noteTitle = $(".note-title");
+const $noteText = $(".note-textarea");
+const $saveNoteBtn = $(".save-note");
+const $newNoteBtn = $(".new-note");
+const $noteList = $(".list-container .list-group");
 
 // activeNote is used to keep track of the note in the textarea
-var activeNote = {};
+let activeNote = {};
 
 // A function for getting all notes from the db
-var getNotes = function() {
+let getNotes = function() {
   return $.ajax({
     url: "/api/notes",
     method: "GET"
---
+++ server.js
@@ -0,0 +1,40 @@
+//Dependecies
+var express = require("express");
+var path = require("path");
+
+//Set up Express App
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+//Handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//needed variables
+
+//Routes
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "styles.css"));
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.js"));
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "styles.css"));
+});
+
+//Server is listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+    console.log("good luck");
+  });
